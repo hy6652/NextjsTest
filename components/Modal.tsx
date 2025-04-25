f5afe250943ea0b0b54332014645df61965f149c
고현영 <hyko@langcode.io>
2025-04-25 15:26:25 +0900

    모달 수정

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -3,22 +3,22 @@ import { faXmark } from '@fortawesome/free-solid-svg-icons';
 import { ReactNode } from "react";
 
 interface ModalProps{
-    title: string;
-    cancelBtn: string;
-    confirmBtn: string;
-    onClose: () => void;
-    onConfirm: () => void;
+    modalHeading: string;
+    primaryButtonText: string;
+    secondaryButtonText: string;
+    onRequestClose: () => void;
+    onRequestSubmit: () => void;
     children?: ReactNode;
 }
 
-export default function CommonModal({ title, cancelBtn, confirmBtn, onClose, onConfirm, children }: ModalProps){
+export default function Modal({ modalHeading, primaryButtonText, secondaryButtonText, onRequestClose, onRequestSubmit, children }: ModalProps){
     return(
         <div className="modal-container" style={{ display: "flex" }} id="modal1">
             <div className="modal-wrap">
                 <div className="modal-content">
                     <div className="modal-content-header pr0">
-                        <p className="modal-title">{title}</p>
-                        <button className="btn btn-transparent btn-sm" onClick={onClose}>
+                        <p className="modal-title">{modalHeading}</p>
+                        <button className="btn btn-transparent btn-sm" onClick={onRequestClose}>
                             <FontAwesomeIcon icon={faXmark}/>
                         </button>
                     </div>
@@ -27,8 +27,8 @@ export default function CommonModal({ title, cancelBtn, confirmBtn, onClose, onC
                     </div>
                 </div>
                 <div className="modal-btn-wrap">
-                    <button className="btn btn-default btn-lg" onClick={onClose}>{cancelBtn}</button>
-                    <button className="btn btn-primary btn-lg" onClick={onConfirm}>{confirmBtn}</button>
+                    <button className="btn btn-default btn-lg" onClick={onRequestClose}>{secondaryButtonText}</button>
+                    <button className="btn btn-primary btn-lg" onClick={onRequestSubmit}>{primaryButtonText}</button>
                 </div>
             </div>
         </div>
